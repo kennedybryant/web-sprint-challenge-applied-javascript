4cd7a4de17028ce52fb6b68185f447b1decd4bc5
kennedybryant <kennedy_marisa96@yahoo.com>
2021-08-06 13:34:29 -0400

    All Complete Except Task 6

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios"
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -55,6 +57,14 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  // axios.get('http://localhost:5000/api/articles')
+  // .then(res => {
+  //   const cardElements = Array.from(res.articles)
+  //   console.log(cardElements)
+  //   })
+  //   .catch(err => {
+  //     console.error(err)
+  //   })
 }
 
 export { Card, cardAppender }
